d98e2c980262ad945bbf372c9e1a8aa4c6c15019
Ayaabudagar <ayakhlil23@yahoo.com>
2021-04-26 21:34:37 +0300

    add new chart

--- app.js
+++ app.js
@@ -129,8 +129,70 @@ function eventButton( e) {
     }
 
     button.removeEventListener('click', eventHandler);
-
+    renderChart();
 }
+function renderChart() {
+
+    let clicks = [];
+    let names = [];
+    let shown = [];
+    for( let i = 0; i < Product.all.length; i++ ) {
+      clicks.push( Product.all[i].clicks );
+      names.push( Product.all[i].name );
+      shown.push( Product.all[i].shown );
+  
+    }
+  
+    let ctx = document.getElementById( 'myChart' ).getContext( '2d' );
+    let myChart = new Chart( ctx, {
+      type: 'bar',
+      data: {
+        labels: names,
+        datasets: [{
+          label: '# of Votes',
+          data: clicks,
+          backgroundColor:
+            'rgba(255, 99, 132, 0.2)',
+          borderColor:
+            'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+        }, {
+          label: '# of shown',
+          data: shown,
+          backgroundColor:
+            'rgba(144, 99, 100, 0.2)',
+          borderColor:
+            'rgba(144, 99, 100, 1)',
+          borderWidth: 1,
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    } );
+   
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 button.addEventListener('click', eventButton);
